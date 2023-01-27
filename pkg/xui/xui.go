64ac13586c20722783ca2099e4610e0d4501e74a
mhshahin <mhshahin91@gmail.com>
2023-01-27 14:33:35 +0330

    Implemented logging

--- pkg/xui/xui.go
+++ pkg/xui/xui.go
@@ -1,6 +1,13 @@
 package xui
 
-import "github.com/mhshahin/xray-tg/pkg/xui/models"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/mhshahin/xray-tg/pkg/config"
+	"github.com/mhshahin/xray-tg/pkg/xui/models"
+)
 
 type XUIInterface interface {
 	Login() error
@@ -10,13 +17,48 @@ type XUIInterface interface {
 	RestartService() error
 }
 
-type XUI struct{}
+type XUI struct {
+	restyClient *resty.Client
+}
 
 func NewXUI() XUIInterface {
-	return &XUI{}
+	restyClient := resty.New()
+	restyClient.SetTimeout(config.Cfg.XUI.Timeout)
+
+	return &XUI{
+		restyClient: restyClient,
+	}
 }
 
 func (x *XUI) Login() error {
+	url := fmt.Sprintf("%s/login", config.Cfg.XUI.Address)
+
+	resp, err := x.restyClient.R().
+		SetBody(&models.LoginRequest{
+			Username: config.Cfg.XUI.Username,
+			Password: config.Cfg.XUI.Password,
+		}).Post(url)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		return fmt.Errorf("Not OK")
+	}
+
+	cookies := []*http.Cookie{
+		{
+			Name:  "lang",
+			Value: "en-US",
+		},
+		{
+			Name:  "session",
+			Value: resp.Header().Values("Set-Cookie")[0],
+		},
+	}
+
+	x.restyClient.SetCookies(cookies)
+
 	return nil
 }
 
